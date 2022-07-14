1cbee9c3b746c8430792ff240e35b34d14a64e57
Hema <hemasahu019@gmail.com>
2022-07-14 10:10:58 +0530

    [Added Abstraction demo program]

---
+++ OOPConcepts/Abstraction/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConcepts.Abstraction
+{
+    //Abstract class
+    abstract class Animal
+    {
+        //Abstract method does not have body
+        public abstract void Sound();
+        public void sleep()
+        {
+            Console.WriteLine("Zzzz....");
+        }
+    }
+}
